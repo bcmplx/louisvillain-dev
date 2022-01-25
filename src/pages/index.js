771ffa1ee6de8b665c252528e0bbf999d681f952
Louis Villain <villainl@hotmail.com>
2022-01-25 12:04:42 +0100

    Intro Section and Navbar

---
+++ src/pages/index.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react';
+
+import Sidebar from '../components/Sidebar';
+import Navbar from '../components/Navbar';
+
+// import TemplateSections from '../components/TemplateSections';
+// import { profil, contact } from '../components/SectionsData/Data';
+
+import IntroSection  from '../components/IntroSection';
+// import FormationsSection from '../components/FormationsSection';
+// import CompetencesSection from '../components/CompetencesSection';
+// import RealisationsSection from '../components/RealisationsSection';
+// import Footer from '../components/Footer';
+
+const Home = () => {
+
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggle = () => {
+		setIsOpen(!isOpen);
+	};
+
+	return (
+		<>
+			
+			<Sidebar isOpen={isOpen} toggle={toggle}/>
+			 <Navbar toggle={toggle}/>
+			<IntroSection />
+			{/*
+			<TemplateSections {...profil} />
+			<CompetencesSection/>
+			<RealisationsSection />
+			<FormationsSection />
+			<TemplateSections {...contact} />*/}
+			{/* <Footer />  */}
+		</>
+	);
+};
+
+export default Home;
