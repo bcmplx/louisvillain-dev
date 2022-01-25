376666774bfd105e4291f5edcef438073587474f
Louis Villain <villainl@hotmail.com>
2022-01-25 12:59:42 +0100

    Navbar fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+// import 'bootstrap/dist/css/bootstrap.css';
 
 import Home from './pages';
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import TemplateSections from '../components/TemplateSections';
 import { profil, contact } from '../Data/Data';
 
 import IntroSection  from '../components/IntroSection';
-// import FormationsSection from '../components/FormationsSection';
+import FormationsSection from '../components/FormationsSection';
 import CompetencesSection from '../components/CompetencesSection';
 import RealisationsSection from '../components/RealisationsSection';
 import Footer from '../components/Footer';
@@ -32,9 +32,9 @@ const Home = () => {
 			<CompetencesSection/>
 			
 			<RealisationsSection />
-			{/*
+			
 			<FormationsSection />
-			*/}
+			
 			<TemplateSections {...contact} />
 			<Footer /> 
 		</>
